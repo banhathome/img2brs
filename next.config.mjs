079ee2f50878303955357d7b5c0a491421977429
Chris Banh <chris@chrisbanh.com>
2025-07-16 10:20:37 -0700

    Changes for Next.js static gen for GH pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '/img2brs-js',
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
